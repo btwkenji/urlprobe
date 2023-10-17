7123aa0d17e0f7e70444b6feb8f1f03bee5d2722
kenjitheman <kenjith3man@gmail.com>
2023-10-17 22:00:06 +0300

    feat: implement read fns for csv, json, txt

--- src/read.rs
+++ src/read.rs
@@ -1,27 +1,29 @@
+use csv;
 use serde_json;
 use std::error::Error;
 use std::fs;
 
 pub fn read_urls_from_json(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    let file = fs::read_to_string(file_path)?;
-    let urls: Vec<String> = serde_json::from_str(&file)?;
+    let contents = fs::read_to_string(file_path)?;
+    let urls: Vec<String> = serde_json::from_str(&contents)?;
     Ok(urls)
 }
 
 pub fn read_urls_from_csv(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    let mut urls = Vec::new();
-    let mut rdr = csv::ReaderBuilder::new().from_path(file_path)?;
+    let mut urls = vec![];
+    let file = fs::File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+
     for result in rdr.records() {
         let record = result?;
-        if let Some(url) = record.get(0) {
-            urls.push(url.to_string());
-        }
+        urls.push(record.get(0).unwrap().to_string());
     }
+
     Ok(urls)
 }
 
 pub fn read_urls_from_txt(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let file = fs::read_to_string(file_path)?;
-    let urls: Vec<String> = file.lines().map(|s| s.to_string()).collect();
+    let urls: Vec<String> = file.lines().map(str::to_string).collect();
     Ok(urls)
 }
