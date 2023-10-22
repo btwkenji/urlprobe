3f52118cefb5fc42ab6aa7208e62bda42b9a0790
kenjitheman <kenjitheman@proton.me>
2023-10-22 23:59:03 +0300

    feat: read.rs implemented (#2)

--- src/read.rs
+++ src/read.rs
@@ -1,29 +1,45 @@
-use csv;
-use serde_json;
 use std::error::Error;
 use std::fs;
 
 pub fn read_urls_from_json(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let contents = fs::read_to_string(file_path)?;
-    let urls: Vec<String> = serde_json::from_str(&contents)?;
-    Ok(urls)
+    let data: Result<serde_json::Value, _> = serde_json::from_str(&contents);
+    match data {
+        Ok(value) => {
+            if let Some(urls) = value.get("urls") {
+                if let Some(urls) = urls.as_array() {
+                    let urls: Vec<String> = urls
+                        .iter()
+                        .filter_map(|u| u.as_str())
+                        .map(String::from)
+                        .collect();
+                    Ok(urls)
+                } else {
+                    Err("invalid JSON structure: 'urls' is not an array".into())
+                }
+            } else {
+                Err("invalid JSON structure: 'urls' key is missing".into())
+            }
+        }
+        Err(e) => Err(e.into()),
+    }
 }
 
 pub fn read_urls_from_csv(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut urls = vec![];
     let file = fs::File::open(file_path)?;
     let mut rdr = csv::Reader::from_reader(file);
-
     for result in rdr.records() {
         let record = result?;
-        urls.push(record.get(0).unwrap().to_string());
+        if let Some(url) = record.get(0) {
+            urls.push(url.to_string());
+        }
     }
-
     Ok(urls)
 }
 
 pub fn read_urls_from_txt(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let file = fs::read_to_string(file_path)?;
-    let urls: Vec<String> = file.lines().map(str::to_string).collect();
+    let urls: Vec<String> = file.lines().map(String::from).collect();
     Ok(urls)
 }
